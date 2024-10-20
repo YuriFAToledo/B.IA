41bf74fcdaa21b5b85b337677616f65a4e154b96
YuriFAToledo <yuri.toledo@sou.inteli.edu.br>
2024-10-20 04:12:18 -0300

    feat: update code

--- src/utils/wcagService.ts
+++ src/utils/wcagService.ts
@@ -16,14 +16,49 @@ export function getWcagCriteria(
 
 	// Parse the requirements string into an object
 	// Load the wcag-criteria.json file
-	const wcagCriteriaPath = path.resolve(
-		__dirname,
-		"../constants/wcag-criteria.json"
-	)
+	// const wcagCriteriaPath = path.resolve(
+	// 	__dirname,
+	// 	"../constants/wcag-criteria.json"
+	// )
 	let wcagData: {[publicName: string]: any[]}
 	try {
-		const data = fs.readFileSync(wcagCriteriaPath, "utf8")
-		wcagData = JSON.parse(data)
+		// const data = fs.readFileSync(wcagCriteriaPath, "utf8")
+
+		wcagData = {
+      "totalBlindness": [
+        {
+          "id": "1.1.1",
+          "name": "Non-text Content",
+          "level": "A",
+          "description": "Provide text alternatives for all non-text content."
+        },
+        {
+          "id": "1.3.1",
+          "name": "Information and Relationships",
+          "level": "A",
+          "description": "Ensure that information structure can be programmatically determined."
+        },
+        {
+          "id": "2.1.1",
+          "name": "Keyboard",
+          "level": "A",
+          "description": "All content must be operable via keyboard."
+        },
+        {
+          "id": "3.3.2",
+          "name": "Labels or Instructions",
+          "level": "A",
+          "description": "Provide descriptive labels for data inputs."
+        },
+        {
+          "id": "4.1.2",
+          "name": "Name, Role, Value",
+          "level": "A",
+          "description": "User interface components must have programmatically defined name, role, and value."
+        }
+      ]
+    }
+    
 	} catch (error) {
     core.setFailed("Error reading wcag-criteria.json file.")
 		throw new Error("Error reading wcag-criteria.json file.")
